8200278324e7f40fe34a96ecda69e0719d924540
uphonia <wu.angela96@icloud.com>
2023-03-25 20:06:10 -0700

    initial home page layout

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
+
+import { Wrapper, Header, Subheader, AddButton } from "./home.styles";
 
 export const Home = () => (
-  <div>Home</div>
-)
+  <Wrapper>
+    <Header>Welcome!</Header>
+    <Subheader>There are currently no recipes added. Get started here!</Subheader>
+    <AddButton>Add Recipe</AddButton>
+  </Wrapper>
+);
---
+++ src/Home/home.styles.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  align-items: center;
+  background-color: #559A90;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  padding: 0px 30px;
+  text-align: center;
+`;
+
+export const Header = styled.h1`
+  color: white;
+`
+
+export const Subheader = styled.h2`
+  color: white;
+`;
+
+export const AddButton = styled.button``
