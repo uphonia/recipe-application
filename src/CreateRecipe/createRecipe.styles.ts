47ec6ca8a23d9b87a9b9d312b14d4354002ae58e
uphonia <wu.angela0@gmail.com>
2026-05-11 14:35:04 -0700

    set background of main layout content

--- src/CreateRecipe/createRecipe.styles.ts
+++ src/CreateRecipe/createRecipe.styles.ts
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  background-color: #737e81;
   display: flex;
   flex: 1 0 auto;
   flex-direction: column;
--- src/common/components/Layout/layout.styles.ts
+++ src/common/components/Layout/layout.styles.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { mq } from "../../utils/mediaQueries";
 
 export const MainWrapper = styled.div`
+  background-color: #737e81;
   display: flex;
   flex-direction: column;
   height: 100%;
