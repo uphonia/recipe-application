ac1182a0f48a1fe28555c50e3cb42c6d834086a4
uphonia <wu.angela0@gmail.com>
2026-05-12 10:13:58 -0700

    remove unused components

--- src/common/components/FormCheckbox/formCheckbox.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Wrapper = styled.div`
-  align-items: center;
-  display: flex;
-`;
-
-export const Checkbox = styled.input``;
--- src/common/components/Link/link.styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled'
-
-
-export const LinkComponent = styled.a`
-  text-decoration: none;
-`
