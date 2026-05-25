5640a253a00cdb77254c4280284f8b5efc77d22d
uphonia <wu.angela0@gmail.com>
2026-05-25 15:23:06 -0700

    get request to fetch recipes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
-    path('recipes/create/', views.create_recipe, name='create_recipe')
+    path('recipes/create/', views.create_recipe, name='create_recipe'),
+    path('recipes/', views.get_recipes, name='get_recipes')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -16,3 +16,11 @@ def create_recipe(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, stataus=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_recipes(request):
+    recipes = Recipe.objects.all()
+    serializer = RecipeSerializer(recipes, many=True)
+    return Response(serializer.data)
+
+    
