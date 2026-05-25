be1f81b414085b6429d7dfa54874f8ec538dd967
uphonia <wu.angela0@gmail.com>
2026-05-25 15:20:36 -0700

    post request to create a new recipe

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
+    path('recipes/create/', views.create_recipe, name='create_recipe')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Recipe
+from .serializers import RecipeSerializer
 
 @api_view(['GET'])
 def test(request):
     return Response({'message': 'Django is connected'})
+
+@api_view(['POST'])
+def create_recipe(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, stataus=status.HTTP_400_BAD_REQUEST)
