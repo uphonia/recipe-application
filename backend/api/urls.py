84605bfa87595f67134d7e431997a3f87d03ea41
uphonia <wu.angela0@gmail.com>
2026-05-28 13:58:26 -0700

    add endpoint path for Favorites table actions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('recipes/create/', views.create_recipe, name='create_recipe'),
     path('recipes/', views.get_recipes, name='get_recipes'),
     path('recipes/<int:id>/', views.get_recipe, name='get_recipe'),
-    path('recipes/<int:id>/delete/', views.delete_recipe, name='delete_recipe')
+    path('recipes/<int:id>/delete/', views.delete_recipe, name='delete_recipe'),
+    path('favorites/favorite/', views.add_favorite, name='add_favorite'),
+    path('favorites/unfavorite/', views.unfavorite, name='unfavorite'),
 ]
