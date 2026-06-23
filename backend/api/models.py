01361e3a1c1f8cf0a58738e9ee5afc5c91e869fd
uphonia <wu.angela0@gmail.com>
2026-06-23 13:05:40 -0700

    add new columns for User dependency

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,24 +1,32 @@
 from django.db import models
+from django.conf import settings
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
-    servings = models.TextField()
+    servings = models.PositiveIntegerField(null=True, blank=True)
     ingredients = models.TextField()
     instructions = models.TextField()
     image = models.ImageField(upload_to='recipes/', blank=True, null=True)
     blurb = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="recipes"
+    )
 
     def __str__(self):
         return self.name
 
-# ToDo - add user_id column
 class Favorites(models.Model):
     favorited = models.BooleanField()
-    recipe_id = models.ForeignKey(
+    recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
         related_name="favorites"
     )
-
-# ToDo - add user column
+    favorited_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="favorites"
+    )
